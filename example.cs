96220b44303c41db3e9ab78669cf75262c2b1e6b
Gabriel Stoimenov <gabrielstoimenov00@gmail.com>
2025-03-02 18:46:45 +0200

    Create example.cs
    new task

---
+++ example.cs
@@ -0,0 +1,10 @@
+int number;
+bool number2 = 100 < int.Parse(Console.ReadLine());
+if (number2)
+{
+    Console.WriteLine("More than 100 .");
+}
+else
+{
+    Console.WriteLine("Less than 100 .");
+}
